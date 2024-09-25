b7bc0b2f117182f35287d771380e4237959ce226
Septian Adi Pratama <septianadip7998@gmail.com>
2024-09-25 13:47:24 +0700

    login showed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import Login from "./components/Login";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline text-end">
-      Hello world!
-    </h1>
-  )
+    <>
+      <h1 className="text-3xl font-bold underline text-end">Hello world!</h1>
+      <div>
+        <Login />
+      </div>
+    </>
+  );
 }
